86f049d8d911344e9268466692512449363cfdac
matsalinas <matias.salinas.s@mail.pucv.cl>
2020-01-08 17:35:21 +0000

    Add index on models

--- eol_feedback/models.py
+++ eol_feedback/models.py
@@ -4,13 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 class EolFeedback(models.Model):
-    block_id = models.CharField(max_length=50, unique=True)
+    block_id = models.CharField(max_length=50, unique=True, db_index=True)
     block_feedback = models.TextField()
 
     def __str__(self):
         return self.block_id
 
 class SectionVisibility(models.Model):
+    class Meta:
+        index_together = [
+            ["section_id", "course_id"],
+        ]
+        unique_together = [
+            ["section_id", "course_id"],
+        ]
+        
     section_id = models.CharField(max_length=50)
     course_id = models.CharField(max_length=50)
     is_visible = models.BooleanField(default=False)
